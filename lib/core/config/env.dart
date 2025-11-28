f8102525f94a2fee59003acf864d49d9d8c13f4a
m.abdelfattah <m.abdelfatah.fci@gmail.com>
2025-11-28 15:52:19 +0200

    set env obfuscate

--- lib/core/config/env.dart
+++ lib/core/config/env.dart
@@ -4,32 +4,32 @@ part 'env.g.dart';
 
 @Envied(path: '.env.dev', name: 'EnvDev')
 abstract class EnvDev {
-  @EnviedField(varName: 'BASE_URL')
-  static const String baseUrl = _EnvDev.baseUrl;
-  @EnviedField(varName: 'API_KEY')
-  static const String apiKey = _EnvDev.apiKey;
+  @EnviedField(varName: 'BASE_URL', obfuscate: true)
+  static final String baseUrl = _EnvDev.baseUrl;
+  @EnviedField(varName: 'API_KEY', obfuscate: true)
+  static final String apiKey = _EnvDev.apiKey;
 }
 
 @Envied(path: '.env.prod', name: 'EnvProd')
 abstract class EnvProd {
-  @EnviedField(varName: 'BASE_URL')
-  static const String baseUrl = _EnvProd.baseUrl;
-  @EnviedField(varName: 'API_KEY')
-  static const String apiKey = _EnvProd.apiKey;
+  @EnviedField(varName: 'BASE_URL', obfuscate: true)
+  static final String baseUrl = _EnvProd.baseUrl;
+  @EnviedField(varName: 'API_KEY', obfuscate: true)
+  static final String apiKey = _EnvProd.apiKey;
 }
 
 @Envied(path: '.env.stage', name: 'EnvStage')
 abstract class EnvStage {
-  @EnviedField(varName: 'BASE_URL')
-  static const String baseUrl = _EnvStage.baseUrl;
-  @EnviedField(varName: 'API_KEY')
-  static const String apiKey = _EnvStage.apiKey;
+  @EnviedField(varName: 'BASE_URL', obfuscate: true)
+  static final String baseUrl = _EnvStage.baseUrl;
+  @EnviedField(varName: 'API_KEY', obfuscate: true)
+  static final String apiKey = _EnvStage.apiKey;
 }
 
 @Envied(path: '.env.test', name: 'EnvTest')
 abstract class EnvTest {
-  @EnviedField(varName: 'BASE_URL')
-  static const String baseUrl = _EnvTest.baseUrl;
-  @EnviedField(varName: 'API_KEY')
-  static const String apiKey = _EnvTest.apiKey;
+  @EnviedField(varName: 'BASE_URL', obfuscate: true)
+  static final String baseUrl = _EnvTest.baseUrl;
+  @EnviedField(varName: 'API_KEY', obfuscate: true)
+  static final String apiKey = _EnvTest.apiKey;
 }
--- lib/core/config/env.g.dart
+++ lib/core/config/env.g.dart
@@ -10,34 +10,512 @@ part of 'env.dart';
 // ignore_for_file: type=lint
 // generated_from: .env.dev
 final class _EnvDev {
-  static const String baseUrl = 'https://dev-api.example.com/v1';
+  static const List<int> _enviedkeybaseUrl = <int>[
+    2687541914,
+    669498660,
+    2206547398,
+    3450183018,
+    698026153,
+    3064476995,
+    2354129130,
+    2152746091,
+    3907458100,
+    1823864599,
+    4060609809,
+    1653781122,
+    2924749186,
+    4155826982,
+    3975656514,
+    679548578,
+    3672628651,
+    2404299330,
+    3406992936,
+    534345241,
+    1735652766,
+    2385509643,
+    3579930255,
+    4001048164,
+    1427395600,
+    2889257904,
+    1175744367,
+    2097824131,
+    571626452,
+    729696056,
+  ];
 
-  static const String apiKey = 'dev_api_key_12345';
+  static const List<int> _envieddatabaseUrl = <int>[
+    2687542002,
+    669498704,
+    2206547378,
+    3450182938,
+    698026202,
+    3064477049,
+    2354129093,
+    2152746052,
+    3907458128,
+    1823864690,
+    4060609895,
+    1653781167,
+    2924749283,
+    4155827030,
+    3975656491,
+    679548556,
+    3672628686,
+    2404299322,
+    3406992969,
+    534345332,
+    1735652846,
+    2385509735,
+    3579930346,
+    4001048138,
+    1427395699,
+    2889257951,
+    1175744258,
+    2097824172,
+    571626402,
+    729696009,
+  ];
+
+  static final String baseUrl = String.fromCharCodes(
+    List<int>.generate(
+      _envieddatabaseUrl.length,
+      (int i) => i,
+      growable: false,
+    ).map((int i) => _envieddatabaseUrl[i] ^ _enviedkeybaseUrl[i]),
+  );
+
+  static const List<int> _enviedkeyapiKey = <int>[
+    2349467136,
+    1443447900,
+    3275832269,
+    87239116,
+    3770904859,
+    2234693198,
+    3925326083,
+    2500766772,
+    3599851631,
+    1886281158,
+    2486948336,
+    806119638,
+    2992506432,
+    4012083509,
+    1647896290,
+    191492065,
+    1056975806,
+  ];
+
+  static const List<int> _envieddataapiKey = <int>[
+    2349467236,
+    1443447865,
+    3275832251,
+    87239059,
+    3770904954,
+    2234693182,
+    3925326186,
+    2500766827,
+    3599851524,
+    1886281123,
+    2486948233,
+    806119561,
+    2992506481,
+    4012083463,
+    1647896273,
+    191492053,
+    1056975755,
+  ];
+
+  static final String apiKey = String.fromCharCodes(
+    List<int>.generate(
+      _envieddataapiKey.length,
+      (int i) => i,
+      growable: false,
+    ).map((int i) => _envieddataapiKey[i] ^ _enviedkeyapiKey[i]),
+  );
 }
 
 // coverage:ignore-file
 // ignore_for_file: type=lint
 // generated_from: .env.prod
 final class _EnvProd {
-  static const String baseUrl = 'https://api.example.com/v1';
+  static const List<int> _enviedkeybaseUrl = <int>[
+    3629243554,
+    1310862331,
+    3436967903,
+    2655480413,
+    4198715345,
+    1867357176,
+    3886898071,
+    3305542708,
+    3477813667,
+    2391983336,
+    1515563198,
+    109006335,
+    1670290859,
+    4085598010,
+    564690969,
+    1914090257,
+    2930491580,
+    1018542724,
+    1364433124,
+    4229851006,
+    3114937283,
+    1220004952,
+    3468140057,
+    2033590495,
+    2417336241,
+    2959609164,
+  ];
 
-  static const String apiKey = 'prod_api_key_67890';
+  static const List<int> _envieddatabaseUrl = <int>[
+    3629243594,
+    1310862223,
+    3436967851,
+    2655480365,
+    4198715298,
+    1867357122,
+    3886898104,
+    3305542683,
+    3477813698,
+    2391983256,
+    1515563223,
+    109006289,
+    1670290894,
+    4085598018,
+    564691064,
+    1914090364,
+    2930491596,
+    1018542824,
+    1364433025,
+    4229850960,
+    3114937248,
+    1220004919,
+    3468140148,
+    2033590512,
+    2417336263,
+    2959609213,
+  ];
+
+  static final String baseUrl = String.fromCharCodes(
+    List<int>.generate(
+      _envieddatabaseUrl.length,
+      (int i) => i,
+      growable: false,
+    ).map((int i) => _envieddatabaseUrl[i] ^ _enviedkeybaseUrl[i]),
+  );
+
+  static const List<int> _enviedkeyapiKey = <int>[
+    1997519666,
+    2761225174,
+    2862528819,
+    1722040578,
+    3249941023,
+    1719977908,
+    928777178,
+    1057568625,
+    2288674708,
+    4264031622,
+    2801442517,
+    2456696005,
+    3970405859,
+    1988834538,
+    2983824877,
+    915083733,
+    1546265861,
+    3295892369,
+  ];
+
+  static const List<int> _envieddataapiKey = <int>[
+    1997519682,
+    2761225124,
+    2862528860,
+    1722040678,
+    3249941056,
+    1719977941,
+    928777130,
+    1057568536,
+    2288674763,
+    4264031725,
+    2801442480,
+    2456695996,
+    3970405820,
+    1988834524,
+    2983824858,
+    915083757,
+    1546265916,
+    3295892385,
+  ];
+
+  static final String apiKey = String.fromCharCodes(
+    List<int>.generate(
+      _envieddataapiKey.length,
+      (int i) => i,
+      growable: false,
+    ).map((int i) => _envieddataapiKey[i] ^ _enviedkeyapiKey[i]),
+  );
 }
 
 // coverage:ignore-file
 // ignore_for_file: type=lint
 // generated_from: .env.stage
 final class _EnvStage {
-  static const String baseUrl = 'https://stage-api.example.com/v1';
+  static const List<int> _enviedkeybaseUrl = <int>[
+    591541068,
+    2731787101,
+    3568425163,
+    2686347692,
+    1126911714,
+    1448837655,
+    3313716769,
+    3892982851,
+    3524514383,
+    3334047018,
+    516233565,
+    3563528007,
+    2602980942,
+    1202200803,
+    3412737349,
+    2543418136,
+    1926174756,
+    985413954,
+    3169989849,
+    1986314203,
+    3325898666,
+    1534392644,
+    1649350358,
+    1495543619,
+    2784342437,
+    285449106,
+    2088357907,
+    4171282149,
+    1246175210,
+    741002113,
+    2531291100,
+    2632519090,
+  ];
 
-  static const String apiKey = 'stage_api_key_abcde';
+  static const List<int> _envieddatabaseUrl = <int>[
+    591541028,
+    2731787049,
+    3568425151,
+    2686347740,
+    1126911633,
+    1448837677,
+    3313716750,
+    3892982892,
+    3524514364,
+    3334047070,
+    516233532,
+    3563527968,
+    2602980907,
+    1202200782,
+    3412737316,
+    2543418216,
+    1926174797,
+    985413996,
+    3169989820,
+    1986314147,
+    3325898699,
+    1534392617,
+    1649350310,
+    1495543599,
+    2784342464,
+    285449148,
+    2088358000,
+    4171282058,
+    1246175111,
+    741002158,
+    2531291050,
+    2632519043,
+  ];
+
+  static final String baseUrl = String.fromCharCodes(
+    List<int>.generate(
+      _envieddatabaseUrl.length,
+      (int i) => i,
+      growable: false,
+    ).map((int i) => _envieddatabaseUrl[i] ^ _enviedkeybaseUrl[i]),
+  );
+
+  static const List<int> _enviedkeyapiKey = <int>[
+    1020737143,
+    2824787419,
+    1290214547,
+    2609650834,
+    3092279342,
+    2464826686,
+    2818936666,
+    308563660,
+    3389785939,
+    907391627,
+    2265875265,
+    320687832,
+    4294046640,
+    3912430997,
+    3171047403,
+    3727680985,
+    2927463595,
+    425217245,
+    3996191498,
+  ];
+
+  static const List<int> _envieddataapiKey = <int>[
+    1020737028,
+    2824787375,
+    1290214642,
+    2609650933,
+    3092279371,
+    2464826721,
+    2818936635,
+    308563644,
+    3389785914,
+    907391700,
+    2265875242,
+    320687805,
+    4294046665,
+    3912431050,
+    3171047306,
+    3727680955,
+    2927463624,
+    425217209,
+    3996191599,
+  ];
+
+  static final String apiKey = String.fromCharCodes(
+    List<int>.generate(
+      _envieddataapiKey.length,
+      (int i) => i,
+      growable: false,
+    ).map((int i) => _envieddataapiKey[i] ^ _enviedkeyapiKey[i]),
+  );
 }
 
 // coverage:ignore-file
 // ignore_for_file: type=lint
 // generated_from: .env.test
 final class _EnvTest {
-  static const String baseUrl = 'https://test-api.example.com/v1';
+  static const List<int> _enviedkeybaseUrl = <int>[
+    2986889503,
+    3442081786,
+    1032518704,
+    407195309,
+    3348645876,
+    132704848,
+    2190378157,
+    2270831259,
+    3891807972,
+    3959643303,
+    3365084779,
+    931819368,
+    2553515854,
+    3686891357,
+    2691282096,
+    3660502171,
+    3503698221,
+    2160378563,
+    3009332009,
+    138388618,
+    3769240404,
+    609859864,
+    92469825,
+    3826492049,
+    1240112181,
+    2398074594,
+    4258735761,
+    444581623,
+    3206118477,
+    3085903904,
+    715392304,
+  ];
 
-  static const String apiKey = 'test_api_key_fghij';
+  static const List<int> _envieddatabaseUrl = <int>[
+    2986889591,
+    3442081678,
+    1032518724,
+    407195357,
+    3348645767,
+    132704874,
+    2190378114,
+    2270831284,
+    3891807888,
+    3959643330,
+    3365084696,
+    931819292,
+    2553515875,
+    3686891324,
+    2691282112,
+    3660502258,
+    3503698179,
+    2160378534,
+    3009332049,
+    138388715,
+    3769240377,
+    609859944,
+    92469805,
+    3826492148,
+    1240112155,
+    2398074497,
+    4258735870,
+    444581530,
+    3206118498,
+    3085903958,
+    715392257,
+  ];
+
+  static final String baseUrl = String.fromCharCodes(
+    List<int>.generate(
+      _envieddatabaseUrl.length,
+      (int i) => i,
+      growable: false,
+    ).map((int i) => _envieddatabaseUrl[i] ^ _enviedkeybaseUrl[i]),
+  );
+
+  static const List<int> _enviedkeyapiKey = <int>[
+    3348310971,
+    340209601,
+    3279330006,
+    1931804618,
+    2282866020,
+    1761722340,
+    548271078,
+    75496919,
+    1521487323,
+    2251288000,
+    2450610679,
+    2591818445,
+    3356471696,
+    3312903911,
+    807915008,
+    1979572083,
+    3681762007,
+    1006603813,
+  ];
+
+  static const List<int> _envieddataapiKey = <int>[
+    3348310991,
+    340209572,
+    3279329957,
+    1931804606,
+    2282865979,
+    1761722245,
+    548270998,
+    75496894,
+    1521487236,
+    2251287979,
+    2450610578,
+    2591818420,
+    3356471759,
+    3312903809,
+    807915111,
+    1979571995,
+    3681761982,
+    1006603855,
+  ];
+
+  static final String apiKey = String.fromCharCodes(
+    List<int>.generate(
+      _envieddataapiKey.length,
+      (int i) => i,
+      growable: false,
+    ).map((int i) => _envieddataapiKey[i] ^ _enviedkeyapiKey[i]),
+  );
 }
