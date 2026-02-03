23c4329faf1b6119181958713d8e038a8cd48e33
m.abdelfattah <m.abdelfatah.fci@gmail.com>
2026-02-03 15:35:00 +0200

    refactoring

--- lib/features/splash/presentation/pages/splash_page.dart
+++ lib/features/splash/presentation/pages/splash_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../../core/router/app_router.dart';
 import '../../../../core/di/injection.dart' as injection;
 import '../bloc/splash_cubit.dart';
